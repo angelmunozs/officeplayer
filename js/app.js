caa4221fd61ac46eb5d66a2079305a80a15a5031
angelmunozs <angel.munoz.sagaseta@gmail.com>
2015-05-08 10:32:11 +0200

    ups

--- js/app.js
+++ js/app.js
@@ -4,19 +4,19 @@ var platforms = {
 		clear_link : 'https://www.youtube.com/watch?v=XJOpiTCEM4M',
 		iframe_link : 'https://www.youtube.com/embed/XJOpiTCEM4M?rel=0&showinfo=0',
 		times_clicked : 0,
-		is_collapsed : false
+		is_collapsed : true
 	},
 	soundcloud: {
 		clear_link : 'https://soundcloud.com/djamsy/hold-on',
 		iframe_link : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/136825877&color=ff5500&inverse=false&auto_play=false&show_user=true',
 		times_clicked : 0,
-		is_collapsed : false
+		is_collapsed : true
 	},
 	mixcloud: {
 		clear_link : 'https://www.mixcloud.com/modek/modek-mixtape-for-off-radio-3-spain/',
 		iframe_link : 'https://www.mixcloud.com/widget/iframe/?embed_type=widget_standard&embed_uuid=37b4ad1a-39be-4f76-a3a7-89eb741e8e2e&feed=https%3A%2F%2Fwww.mixcloud.com%2Fmodek%2Fmodek-mixtape-for-off-radio-3-spain%2F&hide_artwork=1&hide_cover=1&hide_tracklist=1&light=1&mini=1&replace=0',
 		times_clicked : 0,
-		is_collapsed : false
+		is_collapsed : true
 	}
 }
 //	Time allowed for the iframe to load, before showing error page (Currently unused)
